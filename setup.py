eb7be099b5c9d78876dc23148a213cda164987e1
arielgodoy <arielgodoy@gmail.com>
2023-06-05 23:06:15 -0400

    pre entrega2 ok, con redistribuible

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Paquete pre entrega 2',
+    version='1.0',
+    description='Este es el primer paquete redistribuible',
+    author='Ariel Godoy',
+    author_email='arielgodoy@gmail.com',
+    packages=['mi_paquete']
+)
